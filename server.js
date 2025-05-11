b530506db17d69c753722572aa8a8dd5375bd6c9
gloriousnetworker <iniubongudofot2000@gmail.com>
2025-05-11 09:36:51 +0100

    First Commit

--- server.js
+++ server.js
@@ -6,12 +6,22 @@ const { handleError } = require('./src/utils/errorHandler');
 
 const app = express();
 
-// Middleware
-app.use(cors());
+typeof process !== 'undefined';
+const corsOptions = {
+  origin: [
+    'http://localhost:5000',                    
+    'http://localhost:3000',                    
+    'https://bigrelief.vercel.app',             
+    'http://bigrelief.com.ng',                  
+    'https://big-relief-backend.vercel.app'     
+  ],
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Welcome endpoint (should be before other routes)
 app.get('/', (req, res) => {
   res.json({
     message: 'Welcome to the NGO Listings API',
@@ -23,15 +33,12 @@ app.get('/', (req, res) => {
   });
 });
 
-// Routes
 app.use('/api/v1', ngoRoutes);
 
-// Error handler
 app.use((err, req, res, next) => {
   handleError(err, res);
 });
 
-// Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
